38ea5e6e0b3776d27428df7d2cb2744ce7adc078
Fajrul Aslim <fajrul.aslim@gmail.com>
2024-05-29 18:55:39 +0700

    02-golang-standard-library

--- 02-golang-standard-library/14-package-time.go
+++ 02-golang-standard-library/14-package-time.go
@@ -28,4 +28,11 @@ func main() {
 	fmt.Println(valueTime.Month())
 	fmt.Println(valueTime.Day())
 	fmt.Println(valueTime.Hour())
+
+	var duration1 time.Duration = 100 * time.Second
+	var duration2 time.Duration = 10 * time.Millisecond
+	var duration3 time.Duration = duration1 - duration2
+
+	fmt.Println(duration3)
+	fmt.Printf("duration : %d\n", duration3)
 }
---
+++ 02-golang-standard-library/17-package-encoding.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	value := "Eko Kurniawan Khannedy"
+
+	encoded := base64.StdEncoding.EncodeToString([]byte(value))
+	fmt.Println(encoded)
+
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		fmt.Println("Error", err.Error())
+	} else {
+		fmt.Println(string(decoded))
+	}
+
+	csvString := "eko,kurniawan,khannedy\n" +
+		"budi,pratama,nugraha\n" +
+		"joko,morro,diah"
+
+	reader := csv.NewReader(strings.NewReader(csvString))
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Println(record)
+	}
+
+	writer := csv.NewWriter(os.Stdout)
+
+	_ = writer.Write([]string{"eko", "kurniawan", "khannedy"})
+	_ = writer.Write([]string{"budi", "pratama", "nugraha"})
+	_ = writer.Write([]string{"joko", "morro", "diah"})
+
+	writer.Flush()
+}
