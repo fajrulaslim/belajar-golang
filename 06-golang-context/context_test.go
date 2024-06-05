992bfc2bc4e01fd5051bd45d2c170d9e84f1277e
Fajrul Aslim <fajrul.aslim@gmail.com>
2024-06-05 21:01:07 +0700

    golang-context

---
+++ 06-golang-context/context_test.go
@@ -0,0 +1,141 @@
+package golang_context
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
+
+type keyType string
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	const (
+		keyB keyType = "b"
+		keyC keyType = "c"
+		keyD keyType = "d"
+		keyE keyType = "e"
+		keyF keyType = "f"
+		keyG keyType = "g"
+	)
+
+	// contextB := context.WithValue(contextA, "b", "B")
+	contextB := context.WithValue(contextA, keyB, "B")
+	contextC := context.WithValue(contextA, keyC, "C")
+
+	contextD := context.WithValue(contextB, keyD, "D")
+	contextE := context.WithValue(contextB, keyE, "E")
+
+	contextF := context.WithValue(contextC, keyF, "F")
+	contextG := context.WithValue(contextF, keyG, "G")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+	fmt.Println(contextG)
+
+	// fmt.Println(contextF.Value("f"))
+	fmt.Println(contextF.Value(keyF))
+	fmt.Println(contextF.Value(keyC))
+	fmt.Println(contextF.Value(keyB))
+	fmt.Println(contextA.Value(keyB))
+}
+
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+				time.Sleep(1 * time.Second) // simulasi slow
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateCounter(ctx)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+
+	cancel() // mengirim sinyal cancel ke context
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
+
+func TestContextWithTimeout(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
+
+func TestContextWithDeadline(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithDeadline(parent, time.Now().Add(5*time.Second))
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
